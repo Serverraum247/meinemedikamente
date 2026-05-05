bcd27c31cfb173e690853a7b08444702ccd4cdf4
Daniel Brussig <daniel.brussig@hagebau.com>
2026-05-05 20:06:45 +0200

    Barrierefreiheit P3: eslint-plugin-react-native-a11y
    - Plugin installiert und konfiguriert (.eslintrc.js)
    - 0 Accessibility-Errors bei Lint-Pruefung
    - Regeln: has-accessibility-props, has-valid-accessibility-role,
      has-valid-accessibility-state, no-nested-touchables
    - has-accessibility-hint auf 'off' (False-Positives bei simplen Inputs)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native'],
+  plugins: ['react-native-a11y'],
+  rules: {
+    // Interaktive Elemente muessen accessibilityProps haben (Label, Role)
+    'react-native-a11y/has-accessibility-props': 'error',
+    // accessibilityHint bei nicht-offensichtlichen Aktionen
+    // Auf 'off' weil einfache Inputs (Name, Bestand) keinen Hint brauchen
+    'react-native-a11y/has-accessibility-hint': 'off',
+    // Gueltige Werte fuer accessibilityRole
+    'react-native-a11y/has-valid-accessibility-role': 'error',
+    // Gueltige Werte fuer accessibilityState
+    'react-native-a11y/has-valid-accessibility-state': 'warn',
+    // Keine verschachtelten Touchables
+    'react-native-a11y/no-nested-touchables': 'error',
+  },
 };
